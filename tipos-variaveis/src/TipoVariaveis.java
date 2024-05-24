956fcbec0264faa9e2b8c0091bf403ea2843ee77
Paulo Sergio <ashnikko5297@gmail.com>
2024-05-23 21:27:34 -0300

    Operadores ternarios são representados por ? (true)  e : (false).

--- tipos-variaveis/src/TipoVariaveis.java
+++ tipos-variaveis/src/TipoVariaveis.java
@@ -1,19 +1,5 @@
 public class TipoVariaveis {
     public static void main(String[] args) throws Exception {
-        double salarioMinimo = 2500;
-
-        short numeroCurto = 1;
-        int numeroNormal = numeroCurto;
-        short numeroCurto2 = (short) numeroNormal;
-
-        int numero = 5;
-
-        numero = 10;
-
-        System.out.println(numero);
-
-        final double VALOR_DE_PI = 3.14;
-
-        
+      
     }
 }
