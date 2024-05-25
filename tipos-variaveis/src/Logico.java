79c20b551ed29adae38a560223b4283c5aacc415
Paulo Sergio <ashnikko5297@gmail.com>
2024-05-24 21:01:11 -0300

    Operações lógicas && (and) e || (or).

--- tipos-variaveis/src/Logico.java
+++ tipos-variaveis/src/Logico.java
@@ -3,7 +3,20 @@ public class Logico {
     public static void main(String[] args) {
         boolean condicao1=true;
 
-        boolean condicao2=false;
+        boolean condicao2=true;
+
+
+        if (condicao1 && (7 > 4 )){
+            System.out.println("as duas condições são verdadeiras");
+        }
+
+        if (condicao1 || condicao2){
+            System.out.println("Uma duas condições é verdadeira");
+        }
+
+        System.out.println("fim");
     }
+
+
     
 }
